ba1540fca61a3d5a1576cbee91070f6b63df0c0c
Savinya Nandakumara <86395209+IT20135720@users.noreply.github.com>
2022-06-04 21:06:38 +0530

    Delete test directory

--- test/averageThesisMarks.js
+++
@@ -1,8 +0,0 @@
-const calcaveragethesismarks = (NoOfGroups, TotalMark) => {
-
-    const average = TotalMark / NoOfGroups
-    return average;
-}
-
-module.exports = calcaveragethesismarks
-
--- test/math.test.js
+++
@@ -1,7 +0,0 @@
-const math = require('./averageThesisMarks')
-test("Should calaculate total number of groups", () => {
-    const average = math(50, 2000)
-    expect(average).toBe(40)
-})
-
-
